1973a65548f1be70725a858d765984310fdd1e1d
Matthew Haneburger <teetertotterotter@gmail.com>
2016-03-19 17:14:17 -0400

    message update

--- src/Message.java
+++ src/Message.java
@@ -1,4 +1,52 @@
-
+/* File: Message.java
+ * Authors: Montana Wong, Justin Tumale, Matthew Haneburger
+ * Constructs message to be sent with start time
+ * */
+//import java.text.SimpleDateFormat; //formatting elapsed time
+import java.util.Date; //use for create time
 public class Message {
-
+	
+	//private SimpleDateFormat date;
+	private String message;
+	private long createTime;
+	
+	//constructor
+	Message(String message, long createTime){
+		createTime = System.nanoTime();
+		this.message = message;
+	}
+	//get message
+	String getMessage(){
+		return message;
+	}
+	//get createTime; probably won't be needed
+	long getCreateTime(){
+		return createTime;
+	}
+	//get date timestamp
+	String getTimestamp(){
+		Date date = new Date();
+		String timestamp = String.format("%tT\n", date);
+		return timestamp;
+	}
+	
+	/*
+	 * Whiteboard Reference:
+	 * if(System.nanoTime() - m1.getCreateTime() <= persistenceTimeThreshold)
+	 * {
+	 * send(m1, p1); -> means we have not yet passed the treshold, therefore 
+	 * 				 -> we can receive out messages
+	 * }
+	 * */
+	
+	//disregard; just to reference
+	/*
+	 * date = new SimpleDateFormat("HH:mm:ss");
+	private long generateElapsed(){
+		long start = System.nanoTime();
+		long end = System.nanoTime();
+		long elapse = start - end;
+		return elapse;
+	}*/
+	
 }
