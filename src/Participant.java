a403bdc464c5a608d0b5c31dcf60b9008bb25741
justintumale <fjetumale@yahoo.com>
2016-03-20 14:33:02 -0400

    fixed ip address and port number configuration and arguments

--- src/Participant.java
+++ src/Participant.java
@@ -24,8 +24,10 @@ public class Participant {
 	
 	public void run(){
 		try {
-
-			this.participantSocket = new Socket(this.IP_coordinator, this.portCoordinator);
+			//this.participantSocket = new Socket(this.IP_coordinator, this.portCoordinator);
+			//run on local host for now
+			this.participantSocket = new 
+					Socket(InetAddress.getLocalHost().getHostName(), this.portCoordinator);
 			
 			//create the multicast listener thread
 			this.listenerCoordinator = new ParticipantListener(this.participantSocket);
@@ -58,7 +60,7 @@ public class Participant {
 		}
 	}
 		
-	public static Participant configuration_parser(String[] args){
+	public static Participant configurationParser(String[] args){
 
 		//usage checking
 		if(args.length != 1){
@@ -76,20 +78,28 @@ public class Participant {
 		int _ID = -1;
 		String _logFileName = null;
 		String _IP_Coordinator = null;
+		String _portCoordinator = null;
+		String _ipAndPortString = null;
 		try{
 			
 			Scanner scanner = new Scanner(file);
 			while(scanner.hasNext()){
 				_ID = scanner.nextInt();
 				_logFileName = scanner.next();
-				_IP_Coordinator = scanner.next();
+				_ipAndPortString = scanner.next();
 			}
 			scanner.close();
+			
+			String[] ipAndPortArray =  _ipAndPortString.split(":");
+			_IP_Coordinator = ipAndPortArray[0];
+			_portCoordinator = ipAndPortArray[1];
+			
 
 		}
 		catch(IOException ioe){
 			ioe.printStackTrace();
 		}
+		
 		/*
 		
 		
@@ -98,19 +108,18 @@ public class Participant {
 		String portNumber = portArray[1];
 		
 		*/
-		
-		
-		
+
 		//checking
 		System.out.println(_ID);
 		System.out.println(_logFileName);
 		System.out.println(_IP_Coordinator);
+		System.out.println(_portCoordinator);
 		
 		return new Participant(_ID, _IP_Coordinator, port, true);
 	}
 		
 	public static void main(String[] args) {
-		Participant P1 = Participant.configuration_parser(args);
+		Participant P1 = Participant.configurationParser(args);
 		P1.run();
 
 	
