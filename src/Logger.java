41b23d459b44ae7d643d8cc826b494a736908306
Montana <montanawong@gmail.com>
2016-03-21 15:27:03 -0400

    Logger skeleton code

--- src/Logger.java
+++ src/Logger.java
@@ -6,19 +6,22 @@
  */
 public class Logger implements Runnable {
 	private String fileName;
+	private String message;
 	
-	public Logger(String fileName){
+	public Logger(String fileName, String message){
 		this.fileName = fileName;
+		this.message = message;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		this.writeToFile();
 		
 	}
 	
 	
 	private void writeToFile(){
-		
+		//take the message passed in constructor and append to the file listed above.
+		// if the file doesn't exist yet, create it, other wise append to it.
 	}
 }
