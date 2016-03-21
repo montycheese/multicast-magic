943d817b9e34fc35390ba0b7a3887e4725eec901
Montana <montanawong@gmail.com>
2016-03-21 10:55:46 -0400

    Added the use of logger thread

---
+++ src/Logger.java
@@ -0,0 +1,24 @@
+
+/**
+ * Logs multicast messages from Coordinator to Participant to a specified file
+ * @author montanawong
+ *
+ */
+public class Logger implements Runnable {
+	private String fileName;
+	
+	public Logger(String fileName){
+		this.fileName = fileName;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	private void writeToFile(){
+		
+	}
+}
--- src/ParticipantListener.java
+++ src/ParticipantListener.java
@@ -3,28 +3,49 @@
  * Listens to commands coming from Coordinator. Runs on an infinite loop.
  * 
  * */
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ParticipantListener extends Thread{
 	
-	public Socket participantSocket = null;
+	private int listenPort;
+	private ServerSocket sock;
+	private Socket coordinatorSocket;
+	private ExecutorService threadPool;
+	private String logFileName;
 	
-	public ParticipantListener(Socket participantSocket){
-		this.participantSocket = participantSocket;
+	public ParticipantListener(int listenPort, String logFileName, ExecutorService threadPool){
+		this.listenPort = listenPort;
+		this.threadPool = threadPool;
+		this.logFileName = logFileName;
 	}
 	
 	@Override
 	public void run(){
-		//try{
-			System.out.println("Thread running");
-			//loop that continuously listens for command from coordinator. 
-		//}
-		/*
-		catch(SocketException e){
-			//we close socket on participant class so this thread which is blocking on accept will get an error
-			return;
+		try {
+		this.sock = new ServerSocket(this.listenPort);
+		System.out.println("Listener running on port: " + this.listenPort);
+			while(true){
+					this.coordinatorSocket = this.sock.accept();
+					//TODO HANDLE request with a new custom thread. I called it Logger
+					//Receive message from coordinator here
+					//create logger thread
+					Logger logger = new Logger(this.logFileName);
+					this.threadPool.execute(logger);
+					
+					//may reconsider this design chice for queue message send
+					if (this.coordinatorSocket != null){
+						this.coordinatorSocket.close();
+					}	
+			}
 		}
-		*/
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
