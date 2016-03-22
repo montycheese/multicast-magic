ed4d854284b7b9727b6491783f1a2e471a46983b
justintumale <fjetumale@yahoo.com>
2016-03-22 11:42:14 -0400

    passed in appropriate parameters for the Logger instance in this class

--- src/ParticipantListener.java
+++ src/ParticipantListener.java
@@ -3,7 +3,9 @@
  * Listens to commands coming from Coordinator. Runs on an infinite loop.
  * 
  * */
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -30,10 +32,13 @@ public class ParticipantListener extends Thread{
 		System.out.println("Listener running on port: " + this.listenPort);
 			while(true){
 					this.coordinatorSocket = this.sock.accept();
+					BufferedReader br = new BufferedReader(new InputStreamReader(coordinatorSocket.getInputStream()));
+					String message = br.readLine();
 					//TODO HANDLE request with a new custom thread. I called it Logger
 					//Receive message from coordinator here
 					//create logger thread
-					Logger logger = new Logger(this.logFileName);
+	
+					Logger logger = new Logger(this.logFileName, message);
 					this.threadPool.execute(logger);
 					
 					//may reconsider this design chice for queue message send
